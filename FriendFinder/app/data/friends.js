755f6ac0d53f6def7f4c56f5aaabd5d114d07d8c
Dior <49665409+Diorfuego@users.noreply.github.com>
2019-10-27 18:59:46 -0700

    Update friends.js

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,1 +1,21 @@
-test
+var friendsArr = [
+	{
+	  "name":"Dior",
+	  "photo":"https://www.linkedin.com/in/diorchristian/",
+	  "scores[]":[
+	      5,
+	      1,
+	      4,
+	      4,
+	      5,
+	      1,
+	      2,
+	      5,
+	      4,
+	      1
+	    ]
+	}
+];
+
+
+module.exports = friendsArr;
