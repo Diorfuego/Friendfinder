24d532744d7a93254f00cc61d275d683ca37e80a
Dior <49665409+Diorfuego@users.noreply.github.com>
2019-10-27 18:59:02 -0700

    Update server.js

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,1 +1,23 @@
-test
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+//friends.js will be used in routes js files
+//imports those rouutes
+var apiroutes = require("./app/routing/apiroutes.js");
+var htmlroutes = require("./app/routing/htmlroutes.js");
+
+app.use(apiroutes);
+app.use(htmlroutes);
+
+//listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
